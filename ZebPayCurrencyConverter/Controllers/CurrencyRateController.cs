776f96d1f89837e23380e844d8149da0ae08dbb7
Tejas patel <tpatel@bcssarl.com>
2016-10-22 21:20:56 +0530

    Currency converter changes

---
+++ ZebPayCurrencyConverter/Controllers/CurrencyRateController.cs
@@ -0,0 +1,156 @@
+﻿using RestSharp;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Timers;
+using System.Web.Mvc;
+
+namespace ZebPayCurrencyConverter.Controllers
+{
+    public class CurrencyRateController : Controller
+    {
+        #region "Variables"
+        CurrencyConverterEntities st = new CurrencyConverterEntities();
+        string apiUrl = ConfigurationManager.AppSettings["ApiUrlClient"];
+        decimal latestCurrencyRate = 0.0M; 
+        string googleUrl = ConfigurationManager.AppSettings["GoogleCurrencyConvertUrl"].ToString(), postData = "", toCurrency = "INR";
+        Int32 amount = 1;
+        #endregion
+        // GET: CurrencyRate
+        public ActionResult CurrencyRate()
+        {
+            
+            var getDestinationCountry = st.ssz_country.Where(z => z.zcountry_id != 1).ToList();
+            ViewBag.destinationCountry = getDestinationCountry;
+            return View();
+        }
+
+        public ActionResult GetRate(string currencyCode, decimal amount)
+        {
+            try
+            {    
+                var client = new RestClient(apiUrl);
+                var request = new RestRequest("api/v0/rate", Method.POST);
+                request.RequestFormat = DataFormat.Json;
+                request.AddBody(new
+                {
+                    fromCurrencyCode = currencyCode,
+                    currencyAmount = amount
+                });
+
+                //execute the request
+                IRestResponse responseContent = client.Execute(request);
+                return Json(responseContent.Content, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json("#Error");
+            }
+        }
+
+        #region Periodically
+        /// <summary>
+        /// GetConversionRateTimer
+        /// </summary>
+        public void GetConversionRateTimer()
+        {
+            Timer timer = new Timer((60000 * 60 * 4)); // every 4 hours 
+            timer.Elapsed += GetConversionRate;
+            timer.Start();
+        }
+        /// <summary>
+        /// GetConversionRate
+        /// </summary>
+        /// <returns></returns>
+        public void GetConversionRate(Object source, ElapsedEventArgs e)
+        {
+            decimal exchangeRate = 0.0m;
+            try
+            {
+                var getCurrencyRate = st.ssz_country.Where(s => s.zcountry_id != 1).ToList();
+                foreach (var currencyCode in getCurrencyRate)
+                {
+                    string googleCurConverter = googleUrl + amount + "&from=" + currencyCode.zcountry_currency_code.ToString() + "&to=" + toCurrency;
+                    exchangeRate = HttpPostRequest(googleCurConverter);
+                    InsertExchangeRate(Convert.ToByte(currencyCode.zcountry_id), exchangeRate);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        /// <summary>
+        /// HttpPostRequest
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private decimal HttpPostRequest(string url)
+        {
+            try
+            {
+
+                HttpWebRequest myHttpWebRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+                myHttpWebRequest.Method = "POST";
+
+                byte[] data = Encoding.ASCII.GetBytes(postData);
+                myHttpWebRequest.ContentType = "application/x-www-form-urlencoded";
+                myHttpWebRequest.ContentLength = data.Length;
+
+                Stream requestStream = myHttpWebRequest.GetRequestStream();
+                requestStream.Write(data, 0, data.Length);
+                requestStream.Close();
+
+                HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+                Stream responseStream = myHttpWebResponse.GetResponseStream();
+                StreamReader myStreamReader = new StreamReader(responseStream, Encoding.Default);
+                string[] pageContent = Regex.Split(myStreamReader.ReadToEnd(), "&nbsp;");
+                int first = pageContent[1].IndexOf("<div id=currency_converter_result>");
+                int last = pageContent[1].LastIndexOf("</span>");
+
+                string strCurrencyRate = pageContent[1].Substring(first, last - first);
+                string[] currencyValues = strCurrencyRate.Split(' ');
+
+                string strCurrencyValues = currencyValues[5].Remove(0, 10);
+                latestCurrencyRate = Math.Round(Convert.ToDecimal(strCurrencyValues), 2);
+
+                myStreamReader.Close();
+                responseStream.Close();
+                myHttpWebResponse.Close();
+                return latestCurrencyRate;
+            }
+            catch (Exception ex)
+            {
+                return 0.0m;
+            }
+        }
+
+        /// <summary>
+        /// InsertExchangeRate
+        /// </summary>
+        /// <param name="countryId"></param>
+        /// <param name="exchangeRate"></param>
+        public void InsertExchangeRate(byte countryId, decimal exchangeRate)
+        {
+            try
+            {
+                ssz_exchange_rate sse = new ssz_exchange_rate();
+                {
+                    sse.zcountry_id = countryId;
+                    sse.exchange_rate_amount = exchangeRate;
+                    sse.exchange_date = DateTime.Now;
+                    st.ssz_exchange_rate.Add(sse);
+                    st.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            { }
+        }
+
+        #endregion
+    }
+}
---
+++ ZebPayCurrencyConverter/Controllers/v0Controller.cs
@@ -0,0 +1,130 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace ZebPayCurrencyConverter.Controllers
+{
+    public class v0Controller : ApiController
+    {
+        #region "Variables"
+        CurrencyConverterEntities st = new CurrencyConverterEntities();
+        JavaScriptSerializer jsContentDetails;
+        dynamic dataList,totalAmount = 0.0m, conversionRateExist = 0.0M;
+        SendJsonResponse jsonCurrConvertResponse;
+        CurrencyInfo getInfo;
+        #endregion
+
+        #region "methods"
+        
+        [System.Web.Http.HttpPost]
+        public IEnumerable rate(JObject currencyContent)
+        {
+            dataList = currencyContent;
+            jsContentDetails = new JavaScriptSerializer();
+            try
+            {
+                getInfo = jsContentDetails.Deserialize<CurrencyInfo>(currencyContent.ToString());
+                jsonCurrConvertResponse = new SendJsonResponse();
+                // Have done ToUpper() if user enters data outside the website....(E.g. from https://www.hurl.it/).
+                if (getInfo.currencyAmount != 0.0m && getInfo.fromCurrencyCode.ToUpper() != null)
+                {
+                    conversionRateExist = CheckRate(getInfo.fromCurrencyCode.ToUpper());
+                    if (conversionRateExist != 0.0M)
+                    {
+                        totalAmount = CalculateAmount(getInfo.currencyAmount, conversionRateExist);
+                        jsonCurrConvertResponse.SourceCurrency = getInfo.fromCurrencyCode.ToUpper();
+                        jsonCurrConvertResponse.ConversionRate = conversionRateExist;
+                        jsonCurrConvertResponse.Amount = getInfo.currencyAmount;
+                        jsonCurrConvertResponse.Total = totalAmount;
+                        jsonCurrConvertResponse.returncode = 1;
+                        jsonCurrConvertResponse.err = "success";
+                        jsonCurrConvertResponse.timestamp = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                    }
+                }
+                return jsContentDetails.Serialize(jsonCurrConvertResponse);
+            }
+            catch (Exception ex)
+            {
+                jsonCurrConvertResponse.SourceCurrency = getInfo.fromCurrencyCode;
+                jsonCurrConvertResponse.ConversionRate = conversionRateExist;
+                jsonCurrConvertResponse.Amount = getInfo.currencyAmount;
+                jsonCurrConvertResponse.Total = totalAmount;
+                jsonCurrConvertResponse.returncode = 0;
+                jsonCurrConvertResponse.err = "failure";
+                jsonCurrConvertResponse.timestamp = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                return jsContentDetails.Serialize(jsonCurrConvertResponse); ;
+            }
+        }
+
+        /// <summary>
+        /// CheckRate
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="toCurrency"></param>
+        /// <returns></returns>
+        public decimal CheckRate(string toCurrency)
+        {
+            try
+            {
+                var todaysDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day);
+                var currency = st.ssz_exchange_rate.OrderByDescending(x => x.exchange_date).Where(x => x.ssz_country.zcountry_currency_code == toCurrency).FirstOrDefault().exchange_rate_amount;
+                if (currency != null)
+                    return Math.Round(Convert.ToDecimal(currency), 2);
+                else
+                    return 0.0M;
+            }
+            catch (Exception ex)
+            { return 0.0M; }
+        }
+
+        /// <summary>
+        /// CalculateAmount
+        /// </summary>
+        /// <param name="sourceAmount"></param>
+        /// <param name="sourceRate"></param>
+        /// <returns></returns>
+        public decimal CalculateAmount(decimal sourceAmount, decimal sourceRate)
+        {
+            try
+            {
+                if (sourceAmount != 0.0m && sourceRate != 0.0m)
+                    return Math.Round((sourceAmount * sourceRate), 2);
+                else
+                    return 0.0m;
+            }
+            catch (Exception ex)
+            {
+                return 0.0m;
+            }
+        }
+
+        #endregion
+    }
+
+    #region "CurrencyInfo"
+    public class CurrencyInfo
+    {
+        public string fromCurrencyCode { get; set; }
+        public decimal currencyAmount { get; set; }
+    }
+    #endregion
+
+    #region "SendJsonResponse"
+    
+    public class SendJsonResponse
+    {
+        public string SourceCurrency { get; set; }
+        public decimal ConversionRate { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Total { get; set; }
+        public Int16 returncode { get; set; }
+        public string err { get; set; }
+        public string timestamp { get; set; }
+    }
+    #endregion
+}
